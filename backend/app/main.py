3871cbbc7ad536e12d56276af9cae043809ae252
Fabiha Rizvi <142413411+FabihaRizvi@users.noreply.github.com>
2025-08-12 06:05:30 -0700

    improves main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Simulator API")
 
 origins = [
-    "http://localhost:3000",  
+    "http://localhost:3000",
+    "http://127.0.0.1:5173", 
+    "http://localhost:5173", 
 ]
 
 app.add_middleware(
